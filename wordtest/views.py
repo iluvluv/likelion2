187799050fcde6fc3829479be10039d20065c967
somin <somin0416@naver.com>
2019-06-09 02:57:48 +0900

    first commit

---
+++ wordtest/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,"home.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def result(request):
+    text = request.GET['text']
+    words = text.split()
+    dictionary = {}
+    
+    for word in words:
+        if word in dictionary:
+            #increase
+            dictionary[word]+=1
+        else:
+            # add to dictionary
+            dictionary[word]=1    
+
+    return render(request,"result.html", {'full': text, 'total':len(words),'wt':len(text), "word_dictionary" : dictionary.items()})
+    
